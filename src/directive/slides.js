5d40d3a23a3a3d9705c1455f305e6b09412a19ef
marcoliceti <marco.liceti@gmail.com>
2016-05-11 11:02:13 +0200

    More elengant approach (still, duplicate scrolls)

--- src/directive/slides.js
+++ src/directive/slides.js
@@ -27,13 +27,14 @@ angular.module('msl.slides').directive('mslSlides', ['mslSlidesLocation',
       // Slide change logic
 
       scope.changeSlide = function (old_slide_number, new_slide_number) {
-        var start = mslSlidesViewport.positionOf(old_slide_number);
-        var stop = mslSlidesViewport.positionOf(new_slide_number);
+        mslSlidesScrollDetector.stop();
         scope.$emit(
           'msl_slides_slide_change_start',
           old_slide_number,
           new_slide_number
         );
+        var start = mslSlidesViewport.positionOf(old_slide_number);
+        var stop = mslSlidesViewport.positionOf(new_slide_number);
         mslSlidesScroller.scroll(start, stop).then(function () {
           mslSlidesLocation.setSlideNumber(new_slide_number);
           scope.$emit(
@@ -41,17 +42,8 @@ angular.module('msl.slides').directive('mslSlides', ['mslSlidesLocation',
             old_slide_number,
             new_slide_number
           );
+          mslSlidesScrollDetector.start();
         });
-      };
-
-      // Scroll locking
-
-      scope.lockScroll = function (timestamp) {
-        scope.unlock_scroll = timestamp + mslSlidesConfig.duration;
-      };
-
-      scope.scrollLocked = function (timestamp) {
-        return timestamp <= scope.unlock_scroll;
       };
 
       // Event handling
@@ -61,24 +53,22 @@ angular.module('msl.slides').directive('mslSlides', ['mslSlidesLocation',
         if (scope.validSlideNumber(number)) scope.slide_number = number;
       });
 
-      scope.$on('msl_slides_scroll', function (event, direction, timestamp) {
-        if (!scope.scrollLocked(timestamp)) {
-          var next_slide = {
-            up: scope.slide_number - 1,
-            down: scope.slide_number + 1
-          };
-          var number = next_slide[direction];
-          if (scope.validSlideNumber(number)) {
-            scope.$apply(function () {
-              scope.lockScroll(timestamp);
-              scope.slide_number = number;
-            });
-          }
+      scope.$on('msl_slides_scroll', function (event, direction) {
+        var next_slide = {
+          up: scope.slide_number - 1,
+          down: scope.slide_number + 1
         };
+        var number = next_slide[direction];
+        if (scope.validSlideNumber(number)) {
+          scope.$apply(function () {
+            scope.slide_number = number;
+          });
+        }
       });
 
       scope.$watch('slide_number', function (new_slide_number,
         old_slide_number) {
+        scope.slide_number = scope.slide_number || 0;
         var new_slide_number = new_slide_number || 0;
         var old_slide_number = old_slide_number || 0;
         if (new_slide_number !== old_slide_number) {
@@ -90,11 +80,6 @@ angular.module('msl.slides').directive('mslSlides', ['mslSlidesLocation',
 
       mslSlidesScroller.install(mslSlidesConfig);
       mslSlidesScrollDetector.install(scope);
-
-      // Init
-
-      scope.slide_number = 0;
-      scope.lockScroll(0);
     }
   };
 }]);
--- src/service/scroll_detector.js
+++ src/service/scroll_detector.js
@@ -2,27 +2,42 @@
 
 angular.module('msl.slides').factory('mslSlidesScrollDetector', function () {
   return {
-    install: function (scope) {
-      // Mouse & touchpad
-      window.addEventListener('wheel', function (event) {
-        event.preventDefault();
-        var timestamp = event.timeStamp;
+    wheelHandlerFactory: function (scope) {
+      return function (event) {
         if (event.deltaY > 0) {
-          scope.$emit('msl_slides_scroll', 'down', timestamp);
+          scope.$emit('msl_slides_scroll', 'down');
         } else if (event.deltaY < 0) {
-          scope.$emit('msl_slides_scroll', 'up', timestamp);
+          scope.$emit('msl_slides_scroll', 'up');
         }
-      });
-
-      // keyboard
-      document.onkeydown = function (event) {
-        event.preventDefault();
-        var timestamp = event.timeStamp;
+      };
+    },
+    keyboardHandlerFactory: function (scope) {
+      return function (event) {
         var directions = { 40: 'down', 38: 'up' };
         var key = event.keyCode;
         var direction = directions[key];
-        if (direction) scope.$emit('msl_slides_scroll', direction, timestamp);
+        if (direction) scope.$emit('msl_slides_scroll', direction);
       };
+    },
+    start: function () {
+      this.wheelHandler = this.wheelHandlerFactory(this.scope);
+      this.keyboardHandler = this.keyboardHandlerFactory(this.scope);
+      window.addEventListener('wheel', this.wheelHandler);
+      window.addEventListener('keydown', this.keyboardHandler);
+    },
+    stop: function () {
+      window.removeEventListener('wheel', this.wheelHandler);
+      window.removeEventListener('keydown', this.keyboardHandler);
+    },
+    install: function (scope) {
+      this.scope = scope;
+      window.addEventListener('wheel', function (event) {
+        event.preventDefault();
+      });
+      window.addEventListener('keydown', function (event) {
+        event.preventDefault();
+      });
+      this.start();
     }
   };
 });
