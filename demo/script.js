55ac4991fe36f11d1a586a5a4e96161b04ad56ec
marcoliceti <marco.liceti@gmail.com>
2016-05-11 11:11:04 +0200

    Added broadcast example in demo

--- demo/script.js
+++ demo/script.js
@@ -9,9 +9,15 @@ angular.module('demoApp', ['msl.slides']).
     });
     $scope.$on('msl_slides_slide_change_success',
       function (event, old_slide_number, new_slide_number) {
+      $scope.$broadcast('slide_changed');
       console.log(
         'Changed from slide ' + old_slide_number + ' to slide ' +
           new_slide_number
       );
+    });
+  }).
+  controller('SlideCtrl', function ($scope) {
+    $scope.$on('slide_changed', function (event) {
+      console.log('Slide changed');
     });
   });
