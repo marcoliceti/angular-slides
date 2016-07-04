8a0dace2a0a55ad963bc9f4158ebf0c905a86235
marcoliceti <marco.liceti@gmail.com>
2016-07-04 15:47:01 +0200

    Removed mouse-driven slide change

--- src/service/scroll_detector.js
+++ src/service/scroll_detector.js
@@ -2,15 +2,6 @@
 
 angular.module('msl.slides').factory('mslSlidesScrollDetector', function () {
   return {
-    wheelHandlerFactory: function (scope) {
-      return function (event) {
-        if (event.deltaY > 0) {
-          scope.$emit('msl_slides_scroll', 'down');
-        } else if (event.deltaY < 0) {
-          scope.$emit('msl_slides_scroll', 'up');
-        }
-      };
-    },
     keyboardHandlerFactory: function (scope) {
       return function (event) {
         var directions = { 40: 'down', 38: 'up' };
@@ -20,13 +11,10 @@ angular.module('msl.slides').factory('mslSlidesScrollDetector', function () {
       };
     },
     start: function () {
-      this.wheelHandler = this.wheelHandlerFactory(this.scope);
       this.keyboardHandler = this.keyboardHandlerFactory(this.scope);
-      window.addEventListener('wheel', this.wheelHandler);
       window.addEventListener('keydown', this.keyboardHandler);
     },
     stop: function () {
-      window.removeEventListener('wheel', this.wheelHandler);
       window.removeEventListener('keydown', this.keyboardHandler);
     },
     install: function (scope) {
